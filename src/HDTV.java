32684d7570cfcc53a7dfeace9262ccfd2fdb823d
Manju B S <Manju B S@192.168.0.6>
2017-10-15 16:15:00 +0530

    ACD_JAVAB_Session9_Assignment3

---
+++ src/HDTV.java
@@ -0,0 +1,18 @@
+
+/**
+ * 
+ * HDTV class having properties brandname and size
+ *
+ */
+public class HDTV {
+
+	String Brandname;
+	int size;
+
+	// Constructor to assign values to the data members
+	HDTV(String brandname, int size) {
+		this.Brandname = brandname;
+		this.size = size;
+	}
+
+}
